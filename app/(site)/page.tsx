e40f5c2de11f96c069e59ba1d26169eb3e5fe56f
Railson OL <itsrayplay@gmail.com>
2023-08-17 21:49:52 -0300

    Supabase start Config

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,22 @@
 import Header from '@/components/Header'
+import ListItem from '@/components/ListItem'
 
 export default function Home() {
   return (
     <div className="h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
-      <Header>Header</Header>
+      <Header>
+        <h1 className="text-3xl font-semibold text-white">Wellcome back</h1>
+        <div className="mt-3 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          <ListItem image="/images/liked.png" name="Liked songs" href="liked" />
+        </div>
+      </Header>
+
+      <div className="mb-7 mt-2 px-6">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-semibold text-white">Newest Song</h1>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </div>
   )
 }
